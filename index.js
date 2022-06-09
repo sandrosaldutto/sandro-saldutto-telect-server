0d32923fffe198f0da7f0dc5c19100eed506a35c
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-06-09 12:58:51 -0400

    resetting cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors({ origin: "https://telect-client.herokuapp.com/", credentials: true }))
+app.use(cors());
 
 const userRoutes = require("./routes/users");
 app.use("/users", userRoutes);
