d77dd23a08fea56b4e893366ece771f494d0919c
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-12-07 17:37:30 -0700

    final changes maybe

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,21 @@
 const connections = {
   development: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'rootroot',
-      database: 'telect',
-      charset: 'utf8',
+      host: "o2olb7w3xv09alub.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "cj7q22bozv71p33b",
+      password: "lq7u8lcm7gme1yd3",
+      database: "cn897qybgqqwzua5",
+      charset: "utf8",
     },
   },
   production: {
-    client: 'mysql',
+    client: "mysql",
     connection: process.env.JAWSDB_URL,
   },
 };
 
-module.exports = 
-  process.env.NODE_ENV === 'production'
+module.exports =
+  process.env.NODE_ENV === "production"
     ? connections.production
     : connections.development;
